86e802ead8f8d90133b761031f3c0b1a0c597691
Ruxxell <furman06228@gmail.com>
2024-05-29 04:37:35 +0300

    Add models: Dish, DishType, Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Dish Type"
+        verbose_name_plural = "Dish Types"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("kitchen:dish-type-detail", kwargs={"pk": self.pk})
+
+
+class Cook(AbstractUser):
+    prax_years = models.PositiveSmallIntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    class Meta:
+        verbose_name = "Dish"
+        verbose_name_plural = "Dishes"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("kitchen:dish-detail", kwargs={"pk": self.pk})
