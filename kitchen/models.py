2513045382682f3ecb3cdc50c233cbfb559e1e30
Ruxxell <furman06228@gmail.com>
2024-05-29 05:03:41 +0300

    Refactor model Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -19,7 +19,23 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    prax_years = models.PositiveSmallIntegerField(default=0)
+    years_of_experience = models.PositiveSmallIntegerField(default=0)
+
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='cook_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='cook_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
     class Meta:
         verbose_name = "cook"
